75b9b7b8479fe462945e1a54798cf57b0a014d49
Masha Kuznetsova <kmp2535@yandex.ru>
2022-07-26 21:13:45 +0700

    review fix - 2

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,20 +1,21 @@
 export const BASE_URL = 'https://auth.nomoreparties.co';
 
+const getResponse = (response) => {
+  if (response.ok && response.status !== 400 && response.status !== 401) {
+    return response.json();
+  }
+  return Promise.reject(`Что-то пошло не так! Попробуйте ещё раз.`);
+}
+
 export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ email, password })
-    })
-    .then((response) => {
-      if(response.status === 400 || !response.ok){
-        return Promise.reject(`Что-то пошло не так!`);
-      }else{
-        return response.json();
-      }
-    })
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ email, password })
+  })
+    .then(getResponse)
     .then((res) => {
       return res;
     });
@@ -28,16 +29,10 @@ export const login = (email, password) => {
     },
     body: JSON.stringify({ email, password })
   })
-  .then((response) => {
-    if(response.status === 400 || response.status === 401 || !response.ok){
-      return Promise.reject(`Что-то пошло не так! Попробуйте ещё раз.: ${response.status} ${response.statusText}`);
-    }else{
-      return response.json();
-    }
-  })
-  .then((res) => {
-    return res;
-  });
+    .then(getResponse)
+    .then((res) => {
+      return res;
+    });
 }
 
 export const checkToken = (token) => {
@@ -49,11 +44,6 @@ export const checkToken = (token) => {
       'Authorization': `Bearer ${token}`,
     }
   })
-  .then(res => {
-    if(res.ok) {
-      return res.json();
-    }
-    return Promise.reject(`Что-то пошло не так! Попробуйте ещё раз.: ${res.status} ${res.statusText}`);
-  })
-  .then(data => data);
+    .then(getResponse)
+    .then(data => data);
 }
